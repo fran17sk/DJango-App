8819e90120397c1f925752752b33c742d3636ac0
Francisco <francis_cruz2@hotmail.com.ar>
2025-04-21 22:59:32 -0300

    qonda

--- DjangoPrueba/settings.py
+++ DjangoPrueba/settings.py
@@ -136,3 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://innobitsolutions.com',
+]
